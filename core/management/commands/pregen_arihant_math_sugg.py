833a01584b55ef979eba7b776b661f470cdfa429
Aditya Raj <aditya.rajb@gmail.com>
2023-02-22 23:06:22 +0530

    black fmt and add listicle font

--- core/management/commands/pregen_arihant_math_sugg.py
+++ core/management/commands/pregen_arihant_math_sugg.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from core.models import Book, BookPoint
 import fitz
+
 # from PIL import Image
 
 # from core.views import sugg_a_pts
@@ -29,27 +30,41 @@ class Command(BaseCommand):
         for page in o:
             lns = []
             tx = page.get_text("dict")
-            _,_,_, h = page.rect
+            _, _, _, h = page.rect
 
-            for t in tx['blocks']:
+            for t in tx["blocks"]:
 
-                if 'lines' in t and len(t['lines']) > 0 and t['lines'][0]['spans'][0]['font'] in ['Akzidenz-GroteskBQ-Bold', 'Akzidenz-GroteskBQ', 'LiberationSans-Bold']:
+                if (
+                    "lines" in t
+                    and len(t["lines"]) > 0
+                    and t["lines"][0]["spans"][0]["font"]
+                    in [
+                        "Akzidenz-GroteskBQ-Bold",
+                        "Akzidenz-GroteskBQ",
+                        "LiberationSans-Bold",
+                        "Akzidenz-GroteskBQLight",
+                    ]
+                ):
                     # if prev_blk == True:
                     #     lns.pop()
                     #     lns.append(math.floor(t['bbox'][3]) + 1)
                     # else:
-                    lns.append(math.floor(t['bbox'][1]) - 1)
-                    lns.append(math.floor(t['lines'][0]['bbox'][3]) + 1)
+                    lns.append(math.floor(t["bbox"][1]) - 1)
+                    lns.append(math.floor(t["lines"][0]["bbox"][3]) + 1)
 
                     # prev_blk = True
                 # else:
                 #     # prev_blk = False
-            
+
             BookPoint.objects.bulk_create(
-                [BookPoint(book=book, page=page.number + 1, h=pt*100/h) for pt in lns]
-            , ignore_conflicts=True)
+                [
+                    BookPoint(book=book, page=page.number + 1, h=pt * 100 / h)
+                    for pt in lns
+                ],
+                ignore_conflicts=True,
+            )
             leng += len(lns)
 
-        o.close()            
+        o.close()
 
         self.stdout.write(self.style.SUCCESS(f"done! generated {leng} pts"))
